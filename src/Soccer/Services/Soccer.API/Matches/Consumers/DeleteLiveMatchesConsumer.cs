86c91eb8bb86c216ce40e1dc847a5f55b88492cc
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-07-26 11:38:58 +0700

    Add consumers to query api

---
+++ src/Soccer/Services/Soccer.API/Matches/Consumers/DeleteLiveMatchesConsumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.API.Matches.Consumers
+{
+    public class DeleteLiveMatchesConsumer
+    {
+    }
+}
