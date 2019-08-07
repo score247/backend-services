43cc7e9ce0a9ef8f91f242ded76f26e3aa3f59a3
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-08-07 14:49:56 +0700

    Add error handler for event publishers

--- src/Soccer/Services/Soccer.EventPublishers/Startup.cs
+++ src/Soccer/Services/Soccer.EventPublishers/Startup.cs
@@ -1,17 +1,14 @@
 ﻿namespace Soccer.Services.EventPublishers
 {
-    using System;
     using Fanex.Logging;
     using Fanex.Logging.Sentry;
-    using GreenPipes;
-    using MassTransit;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Sentry;
-    using Soccer.EventPublishers.Matches;
+    using Soccer.EventPublishers.Shared.Middlewares;
     using Soccer.EventPublishers.Matches.Hubs;
 
     public class Startup
@@ -26,8 +23,8 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            RegisterLogging(services);
-            RegisterRabbitMq(services);
+            services.AddLogging();
+            services.AddRabbitMq(Configuration);
             services.AddSignalR();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -36,10 +33,7 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.ConfigureExceptionHandler();
 
             app.UseSignalR(routes =>
             {
@@ -51,58 +45,6 @@
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}");
-            });
-        }
-
-        private void RegisterLogging(IServiceCollection services)
-        {
-            LogManager
-                 .SetDefaultLogCategory(Configuration["Fanex.Logging:DefaultCategory"])
-                 .Use(new SentryLogging(new SentryEngineOptions
-                 {
-                     Dsn = new Dsn(Configuration["Fanex.Logging:SentryUrl"])
-                 }));
-
-            services.AddSingleton(Logger.Log);
-        }
-
-        private void RegisterRabbitMq(IServiceCollection services)
-        {
-            services.AddScoped<ProcessMatchEventPublisher>();
-
-            var bus = Bus.Factory.CreateUsingRabbitMq(
-                   cfg =>
-                   {
-                       var host = cfg.Host(Configuration.GetSection("MessageQueue:RabbitMQ:Host").Value, "/", h =>
-                       {
-                           h.Username(Configuration.GetSection("MessageQueue:RabbitMQ:UserName").Value);
-                           h.Password(Configuration.GetSection("MessageQueue:RabbitMQ:Password").Value);
-                       });
-
-                       cfg.ReceiveEndpoint(host, "score247", e =>
-                       {
-                           e.PrefetchCount = 16;
-                           e.UseMessageRetry(x =>
-                           {
-                               x.Interval(2, 100);
-                               x.Handle<Exception>((ex) =>
-                               {
-                                   var logger = services.BuildServiceProvider().GetRequiredService<ILogger>();
-                                   logger.Error(ex.Message, ex);
-
-                                   return true;
-                               });
-                           });
-
-                           e.Consumer(() => services.BuildServiceProvider().GetRequiredService<ProcessMatchEventPublisher>());
-                       });
-                   });
-
-            bus.Start();
-
-            services.AddMassTransit(s =>
-            {
-                s.AddBus(_ => bus);
             });
         }
     }
---
+++ src/Soccer/Services/Soccer.EventPublishers/_Shared/Middlewares/RabbitMqMiddleware.cs
@@ -0,0 +1,69 @@
+﻿namespace Soccer.EventPublishers.Shared.Middlewares
+{
+    using System;
+    using Fanex.Logging;
+    using GreenPipes;
+    using GreenPipes.Configurators;
+    using MassTransit;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Soccer.Core._Shared.Configurations;
+    using Soccer.EventPublishers.Matches;
+
+    public static class RabbitMqMiddleware
+    {
+        public static void AddRabbitMq(this IServiceCollection services, IConfiguration configuration)
+        {
+            var messageQueueSettings = new MessageQueueSettings();
+            configuration.Bind("MessageQueue", messageQueueSettings);
+
+            services.AddScoped<ProcessMatchEventPublisher>();
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(
+                  cfg =>
+                  {
+                      var host = cfg.Host(
+                          messageQueueSettings.Host,
+                          messageQueueSettings.VirtualHost, h =>
+                          {
+                              h.Username(messageQueueSettings.Username);
+                              h.Password(messageQueueSettings.Password);
+                          });
+
+                      cfg.ReceiveEndpoint(host, $"{messageQueueSettings.QueueName}_MatchEvents", e =>
+                      {
+                          e.PrefetchCount = 16;
+                          e.UseMessageRetry(RetryAndLogError(services));
+
+                          e.Consumer(() => services.BuildServiceProvider().GetRequiredService<ProcessMatchEventPublisher>());
+                      });
+                  });
+
+            bus.Start();
+
+            services.AddMassTransit(s =>
+            {
+                s.AddBus(_ => bus);
+            });
+        }
+
+#pragma warning disable S109 // Magic numbers should not be used
+
+        private static Action<IRetryConfigurator> RetryAndLogError(IServiceCollection services)
+        {
+            return x =>
+            {
+                x.Interval(1, 100);
+                x.Handle<Exception>((ex) =>
+                {
+                    var logger = services.BuildServiceProvider().GetRequiredService<ILogger>();
+                    logger.Error(ex.Message, ex);
+
+                    return true;
+                });
+            };
+        }
+
+#pragma warning restore S109 // Magic numbers should not be used
+    }
+}
