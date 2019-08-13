4e4d0b863432a34ab53870327edd53e33d15ec16
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-08-13 16:19:28 +0700

    update sp Match_GetByDateRange

--- database/store-procedures/matches/Match_GetByDateRange.sql
+++ database/store-procedures/matches/Match_GetByDateRange.sql
@@ -1,16 +1,21 @@
 CREATE DEFINER=`user`@`%` PROCEDURE `Match_GetByDateRange`(IN sportId INT, IN fromDate DATETIME, IN toDate DATETIME, IN language TEXT)
 BEGIN
-	(SELECT NonLiveMatch.`Value` 
-    FROM `Match` as NonLiveMatch LEFT JOIN LiveMatch as LiveMatch
-    ON NonLiveMatch.Id != LiveMatch.Id 
-    WHERE NonLiveMatch.SportId = sportId
+	(SELECT NonLiveMatch.`Value`, EventDate, Id 
+    FROM `Match` as NonLiveMatch 
+    WHERE Id NOT IN
+		(
+		SELECT  Id
+		FROM    LiveMatch as LM
+		)
+       AND NonLiveMatch.SportId = sportId
 		AND NonLiveMatch.EventDate >=  fromDate
 		AND NonLiveMatch.EventDate <=  toDate
 		AND NonLiveMatch.`Language` = language)
 	UNION ALL 
-	(SELECT Value FROM `LiveMatch` as LM
+	(SELECT Value, EventDate, Id FROM `LiveMatch` as LM
      WHERE LM.SportId = sportId
 		AND  LM.EventDate >=  fromDate
 		AND LM.EventDate <=  toDate
-		AND LM.`Language` = language);
+		AND LM.`Language` = language)
+	ORDER BY EventDate, Id;
 END
