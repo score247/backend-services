8885f6152d2f4a84a0105b0f12a10a0ee2cbb9d5
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-08-20 14:49:02 +0700

    Update Soccer.EventProcessors to service

--- src/Soccer/Services/Soccer.EventProcessors/Program.cs
+++ src/Soccer/Services/Soccer.EventProcessors/Program.cs
@@ -1,17 +1,54 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-
-namespace Soccer.EventProcessors
+﻿namespace Soccer.EventProcessors
 {
-    public class Program
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Hosting.WindowsServices;
+    using Microsoft.Extensions.Configuration;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                Directory.SetCurrentDirectory(pathToContentRoot);
+            }
+
+            var builder = CreateWebHostBuilder(args.Where(arg => arg != "--console").ToArray());
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+               })
+               .UseConfiguration(configuration)
+               .UseStartup<Startup>()
+               .UseUrls($"http://0.0.0.0:{configuration["Port"]}");
+        }
     }
 }
