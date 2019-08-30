6b2a4c205d52098ee5dc027a83245daa461a52c2
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-08-30 09:17:15 +0700

    Add SP

---
+++ database/store-procedures/matches/LiveMatch_UpdateCurrentPeriodStartTime.sql
@@ -0,0 +1,6 @@
+CREATE DEFINER=`root`@`%` PROCEDURE `LiveMatch_UpdateCurrentPeriodStartTime`(IN sportId INT, IN matchId VARCHAR(45), IN currentPeriodStartTime TINYTEXT)
+BEGIN
+	UPDATE `LiveMatch` as LM
+	SET `Value` = JSON_SET(`Value`,  '$.CurrentPeriodStartTime', JSON_UNQUOTE(currentPeriodStartTime))
+	WHERE LM.`SportId` = sportId AND LM.Id = matchId;
+END
