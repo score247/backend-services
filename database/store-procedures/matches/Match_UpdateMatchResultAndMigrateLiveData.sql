774cd0d0e147a5e41028c71aab48a9c8b035b125
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-09-03 09:23:17 +0700

    update sp Match_UpdateMatchResultAndMigrateLiveData check exist value

--- database/store-procedures/matches/Match_UpdateMatchResultAndMigrateLiveData.sql
+++ database/store-procedures/matches/Match_UpdateMatchResultAndMigrateLiveData.sql
@@ -2,13 +2,18 @@ CREATE DEFINER=`user`@`%` PROCEDURE `Match_UpdateMatchResultAndMigrateLiveData`(
 	IN sportId INT, 
     IN matchId VARCHAR(45), 
     IN matchResult TEXT)
-BEGIN
-	UPDATE `Match` as M
-    SET  M.`Value` = 
-			(SELECT `Value` FROM `LiveMatch` as LM 
-			WHERE LM.SportId = sportId
-			AND LM.Id = matchId);
-        
+BEGIN	
+   IF EXISTS (SELECT 1 FROM `LiveMatch` WHERE SportId = sportId AND Id = matchId) 
+   THEN
+		UPDATE `Match` as M
+		SET  M.`Value` = 
+				(SELECT `Value` FROM `LiveMatch` as LM 
+				WHERE LM.SportId = sportId
+				AND LM.Id = matchId)
+		WHERE M.SportId = sportId
+			AND M.Id = matchId;
+	END IF;
+    
 	UPDATE `Match` as M
     SET  M.`Value` = JSON_REPLACE(Value,  '$.MatchResult', JSON_EXTRACT(matchResult, '$'))
 	WHERE M.SportId = sportId
