292387920f041766baee2c58a1c7efba44fe1f7f
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-09-04 11:19:29 +0700

    0000 - Add testing code for odds movements

--- src/Soccer/Services/Tests/Soccer.EventProcessors.Tests/Odds/OddsChangeConsumerTests.cs
+++ src/Soccer/Services/Tests/Soccer.EventProcessors.Tests/Odds/OddsChangeConsumerTests.cs
@@ -9,8 +9,10 @@
     using Fanex.Data.Repository;
     using Fanex.Logging;
     using MassTransit;
+    using Newtonsoft.Json;
     using NSubstitute;
     using Soccer.Core.Matches.Models;
+    using Soccer.Core.Odds;
     using Soccer.Core.Odds.Models;
     using Soccer.Database.Odds.Criteria;
     using Soccer.EventProcessors.Odds;
@@ -55,6 +57,18 @@
             await dynamicRepository.DidNotReceive().FetchAsync<BetTypeOdds>(Arg.Any<GetOddsCriteria>());
 
 
+        }
+
+        [Fact]
+        public void CheckIssue()
+        {
+            var matchOddsJson = @"{'MatchId':'sr: match: 17380329','LastUpdated':'2019 - 09 - 03T23: 26:19Z','BetTypeOddsList':[{'Id':1,'Name':'1x2','Bookmaker':{'Id':'sr: book: 74','Name':'Bet365'},'LastUpdatedTime':'2019 - 09 - 03T23: 26:19Z','BetOptions':[{'Type':'home','LiveOdds':1.12,'OpeningOdds':1.12,'OptionValue':'','OpeningOptionValue':'','OddsTrend':null},{'Type':'away','LiveOdds':10.00,'OpeningOdds':10.00,'OptionValue':'','OpeningOptionValue':'','OddsTrend':null},{'Type':'draw','LiveOdds':8.00,'OpeningOdds':8.00,'OptionValue':'','OpeningOptionValue':'','OddsTrend':null}]},{'Id':2,'Name':'OverUnder','Bookmaker':{'Id':'sr: book: 74','Name':'Bet365'},'LastUpdatedTime':'2019 - 09 - 03T23: 26:19Z','BetOptions':[{'Type':'over','LiveOdds':1.44,'OpeningOdds':1.44,'OptionValue':'2.5','OpeningOptionValue':'2.5','OddsTrend':null},{'Type':'under','LiveOdds':2.70,'OpeningOdds':2.70,'OptionValue':'2.5','OpeningOptionValue':'2.5','OddsTrend':null}]}]}";
+            var matchJson = "{'EventDate':'2019-09-05T15:00:00+00:00','CurrentPeriodStartTime':'0001-01-01T00:00:00+00:00','Teams':[{'Country':'Georgia','CountryCode':'GEO','Flag':null,'IsHome':true,'Players':null,'Statistic':{'Possession':0,'FreeKicks':0,'ThrowIns':0,'GoalKicks':0,'ShotsBlocked':0,'ShotsOnTarget':0,'ShotsOffTarget':0,'CornerKicks':0,'Fouls':0,'ShotsSaved':0,'Offsides':0,'YellowCards':0,'Injuries':0,'RedCards':0,'YellowRedCards':0},'Coach':null,'Formation':null,'Abbreviation':'GEO','Substitutions':null,'Id':'sr:competitor:6373','Name':'Georgia'},{'Country':'Liechtenstein','CountryCode':'LIE','Flag':null,'IsHome':false,'Players':null,'Statistic':{'Possession':0,'FreeKicks':0,'ThrowIns':0,'GoalKicks':0,'ShotsBlocked':0,'ShotsOnTarget':0,'ShotsOffTarget':0,'CornerKicks':0,'Fouls':0,'ShotsSaved':0,'Offsides':0,'YellowCards':0,'Injuries':0,'RedCards':0,'YellowRedCards':0},'Coach':null,'Formation':null,'Abbreviation':'LIE','Substitutions':null,'Id':'sr:competitor:8030','Name':'Liechtenstein'}],'MatchResult':{'MatchStatus':{'DisplayName':'not_started','Value':1},'EventStatus':{'DisplayName':'not_started','Value':1},'Period':0,'MatchPeriods':null,'MatchTime':0,'WinnerId':null,'HomeScore':0,'AwayScore':0,'AggregateHomeScore':0,'AggregateAwayScore':0,'AggregateWinnerId':null},'League':{'Order':0,'Flag':null,'Category':{'CountryCode':null,'Id':'sr:category:392','Name':'International Youth'},'Id':'sr:tournament:26','Name':'U21 Euro Qualification'},'LeagueRound':{'Type':{'DisplayName':'group','Value':2},'Name':null,'Number':1,'Phase':null,'Group':'2'},'TimeLines':[],'LatestTimeline':null,'Attendance':0,'Venue':{'Capacity':54549,'CityName':'Tbilisi','CountryName':'Georgia','CountryCode':'GEO','Id':null,'Name':'Boris Paichadze Dinamo Arena'},'Referee':null,'Region':'intl','Id':'sr:match:17380329','CreatedTime':'0001-01-01T00:00:00+00:00','ModifiedTime':'0001-01-01T00:00:00+00:00'}";
+
+            var matchOdds = JsonConvert.DeserializeObject<MatchOdds>(matchOddsJson);
+            var match = JsonConvert.DeserializeObject<Match>(matchJson);
+
+            var oddsMovement = OddsMovementProcessor.BuildOddsMovements(match, matchOdds.BetTypeOddsList.ToList(), 2);
         }
     }
 }
