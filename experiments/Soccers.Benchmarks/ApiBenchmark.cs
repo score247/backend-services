e370032bac7f0d645e720a4f705b6cd01e62622f
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-09-04 09:08:04 +0700

    0000 - added benchmarks

---
+++ experiments/Soccers.Benchmarks/ApiBenchmark.cs
@@ -0,0 +1,49 @@
+﻿namespace Soccers.Benchmarks
+{
+    using System;
+    using System.Collections.Generic;
+    using BenchmarkDotNet.Attributes;
+    using RestSharp;
+
+    [InProcessAttribute, MinColumn, MaxColumn]
+    public class ApiBenchmark
+    {
+        private static Dictionary<string, string> Urls = new Dictionary<string, string>
+        {
+            { "New Api", "http://10.19.10.52:51147/api" },
+            { "Old Api", "http://10.19.10.52:51145/api" }
+        };
+
+        public IEnumerable<object[]> GetMatchesBenchmarkParams()
+        {
+            yield return new object[] { "New Api", new DateTime(2019, 08, 28, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "Old Api", new DateTime(2019, 08, 28, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "New Api", new DateTime(2019, 08, 29, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "Old Api", new DateTime(2019, 08, 29, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "New Api", new DateTime(2019, 08, 30, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "Old Api", new DateTime(2019, 08, 30, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "New Api", new DateTime(2019, 08, 31, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "Old Api", new DateTime(2019, 08, 31, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "New Api", new DateTime(2019, 09, 01, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "Old Api", new DateTime(2019, 09, 01, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "New Api", new DateTime(2019, 09, 02, 0, 0, 0, DateTimeKind.Utc) };
+            yield return new object[] { "Old Api", new DateTime(2019, 09, 02, 0, 0, 0, DateTimeKind.Utc) };
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(GetMatchesBenchmarkParams))]
+        public void GetMatchByDate(string apiName, DateTime date)
+        {
+            var client = new RestClient(Urls[apiName]);
+            var request = new RestRequest("/soccer/en-US/matches");
+
+            request.AddParameter("fd", date.Date);
+            request.AddParameter("td", date.AddDays(1).Date.AddMinutes(-1));
+            request.AddParameter("tz", new TimeSpan(7, 0, 0));
+
+            var response = client.Get(request);
+
+            Console.WriteLine($"{apiName} - {date} - {response.RawBytes.Length}");
+        }
+    }
+}
---
+++ experiments/Soccers.Benchmarks/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Soccers.Benchmarks
+{
+    public class Md5VsSha256
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public Md5VsSha256()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        //[Benchmark, Params, ParamsSource, [Arguments(200, 20)]]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+
+    public class IntroSetupCleanupGlobal
+    {
+        [Params(10, 100, 1000)]
+        public int N;
+
+        private int[] data;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            data = new int[N]; // executed once per each N value
+        }
+
+        [Benchmark]
+        public int Logic()
+        {
+            int res = 0;
+            for (int i = 0; i < N; i++)
+                res += data[i];
+            return res;
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            // Disposing logic
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ApiBenchmark>();
+            //var benchmark = new ApiBenchmark();
+            //benchmark.MakeRequest(new DateTime(2019, 08, 30, 0, 0, 0, DateTimeKind.Utc));
+        }
+    }
+}
