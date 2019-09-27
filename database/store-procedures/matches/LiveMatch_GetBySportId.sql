25b11039d88242eecab8559eba989cf14227ef04
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-09-27 18:39:47 +0700

    120869 - filter live match by major leagues

--- database/store-procedures/matches/LiveMatch_GetBySportId.sql
+++ database/store-procedures/matches/LiveMatch_GetBySportId.sql
@@ -3,6 +3,10 @@ DROP procedure IF EXISTS `LiveMatch_GetBySportId`;
 CREATE DEFINER=`user`@`%` PROCEDURE `LiveMatch_GetBySportId`(IN sportId INT, IN language TEXT)
 BEGIN
 	SELECT `Value`
-    FROM LiveMatch as LM
-	WHERE LM.SportId = sportId AND LM.`Language` = language;
+    FROM LiveMatch as LM 
+    INNER JOIN `League` as League ON `Match`.LeagueId = League.Id 	
+	WHERE LM.SportId = sportId 
+		AND LM.`Language` = language 
+        AND League.IsActive = 1
+    ORDER BY League.Order, `Match`.EventDate, `Match`.LeagueId, `Match`.Id;
 END
