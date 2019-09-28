4153375f7146cfcce63d6815a61b07a01e0e61b1
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-09-28 08:08:18 +0700

    120869 - update LiveMatch_GetBySportId

--- database/store-procedures/matches/LiveMatch_GetBySportId.sql
+++ database/store-procedures/matches/LiveMatch_GetBySportId.sql
@@ -4,9 +4,9 @@ CREATE DEFINER=`user`@`%` PROCEDURE `LiveMatch_GetBySportId`(IN sportId INT, IN 
 BEGIN
 	SELECT `Value`
     FROM LiveMatch as LM 
-    INNER JOIN `League` as League ON `Match`.LeagueId = League.Id 	
+    INNER JOIN `League` as League ON LM.LeagueId = League.Id 	
 	WHERE LM.SportId = sportId 
 		AND LM.`Language` = language 
         AND League.IsActive = 1
-    ORDER BY League.Order, `Match`.EventDate, `Match`.LeagueId, `Match`.Id;
+    ORDER BY League.Order, LM.EventDate, LM.LeagueId, LM.Id;
 END
