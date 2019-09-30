e8a387cdb26ced911b5f14c8b63d2a28d29a1302
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-09-30 19:02:05 +0700

    121040 - SP - Update

--- database/store-procedures/matches/Match_UpdateMatchResultAndMigrateLiveData.sql
+++ database/store-procedures/matches/Match_UpdateMatchResultAndMigrateLiveData.sql
@@ -5,8 +5,12 @@ CREATE DEFINER=`user`@`%` PROCEDURE `Match_UpdateMatchResultAndMigrateLiveData`(
     IN matchId VARCHAR(45), 
     IN matchResult TEXT)
 BEGIN	
-   IF EXISTS (SELECT 1 FROM `LiveMatch` WHERE SportId = sportId AND Id = matchId) 
+   IF EXISTS (SELECT * FROM `LiveMatch` WHERE SportId = sportId AND Id = matchId FOR SHARE) 
    THEN
+		UPDATE `LiveMatch` as LM
+		SET  LM.`Value` = JSON_REPLACE(Value,  '$.MatchResult', JSON_EXTRACT(matchResult, '$'))
+		WHERE LM.SportId = sportId AND LM.Id = matchId;
+        
 		UPDATE `Match` as M
 		SET  M.`Value` = 
 				(SELECT `Value` FROM `LiveMatch` as LM 
@@ -15,9 +19,4 @@ BEGIN
 		WHERE M.SportId = sportId
 			AND M.Id = matchId;
 	END IF;
-    
-	UPDATE `Match` as M
-    SET  M.`Value` = JSON_REPLACE(Value,  '$.MatchResult', JSON_EXTRACT(matchResult, '$'))
-	WHERE M.SportId = sportId
-		AND M.Id = matchId;
 END
