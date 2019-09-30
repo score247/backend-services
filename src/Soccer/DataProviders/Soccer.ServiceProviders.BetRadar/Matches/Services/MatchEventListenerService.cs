32fafebf4034612ffe6756b75f1853fe5f513d90
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-09-30 11:14:28 +0700

    121095	- update retry task for event listener

--- src/Soccer/DataProviders/Soccer.ServiceProviders.BetRadar/Matches/Services/MatchEventListenerService.cs
+++ src/Soccer/DataProviders/Soccer.ServiceProviders.BetRadar/Matches/Services/MatchEventListenerService.cs
@@ -18,11 +18,14 @@
     {
         private const int MillisecondsTimeout = 10 * 1000;
         private const int FiveMinutes = 5;
+        private const byte MaxRetryTimes = 5;
+        private static byte retryCount;
         private readonly SportSettings soccerSettings;
         private readonly ISportRadarSettings sportRadarSettings;
         private readonly ILogger logger;
         private readonly Dictionary<string, StreamReader> regionStreams;
-        private bool isWroteHeartbeatLog = false;
+        private bool isWroteHeartbeatLog;
+        private IEnumerable<Task<Task>> runningTasks;
 
         public MatchEventListenerService(ISportRadarSettings sportRadarSettings, ILogger logger)
         {
@@ -41,16 +44,33 @@
 
             try
             {
-                var tasks = soccerSettings?.Regions?.Select(region =>
-                    Task.Factory.StartNew(
-                        () => ListeningEventForRegion(region.Name, region.PushKey, handler, cancellationToken),
-                        cancellationToken));
+#pragma warning disable S2696 // Instance members should not write to "static" fields
+                retryCount++;
+#pragma warning restore S2696 // Instance members should not write to "static" fields
+                runningTasks = soccerSettings?.Regions?.Select(
+                    region => Task.Factory.StartNew(
+                                () => ListeningEventForRegion(region.Name, region.PushKey, handler, cancellationToken),
+                    cancellationToken));
 
-                await Task.WhenAll(tasks);
+                await Task.WhenAll(runningTasks);
+            }
+            catch (AggregateException ex)
+            {
+                await logger.ErrorAsync($"Error in Start Listen Events: {ex.Message}", ex);
             }
             catch (Exception ex)
             {
                 await logger.ErrorAsync($"Error in Start Listen Events: {ex.Message}", ex);
+            }
+            finally
+            {
+                if(retryCount < MaxRetryTimes)
+                {
+                    var tasksInformation = string.Join("\r\n", runningTasks.Select(runningTask => $"TaskId: {runningTask.Id}, Status: {runningTask.Status}"));
+                    await Task.Delay(MillisecondsTimeout, cancellationToken);
+                    await logger.InfoAsync($"Retry {retryCount} time for event listener at {DateTime.Now}.\r\nOld task info:{tasksInformation}");
+                    await ListenEvents(handler, cancellationToken);
+                }
             }
         }
 
