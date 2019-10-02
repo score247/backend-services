35bb8238d56ce3211a9d58ed8d03a45d9ab9d4b4
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-10-02 19:28:10 +0700

    00000 - Add log insert live matches

--- src/Soccer/Services/Soccer.EventProcessors/Matches/FetchedLiveMatchConsumer.cs
+++ src/Soccer/Services/Soccer.EventProcessors/Matches/FetchedLiveMatchConsumer.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Fanex.Data.Repository;
+using Fanex.Logging;
 using MassTransit;
 using Soccer.Core.Matches.Events;
 using Soccer.Core.Matches.Models;
@@ -20,17 +22,20 @@ namespace Soccer.EventProcessors.Matches
         private readonly IBus messageBus;
         private readonly IFilter<IEnumerable<Match>, IEnumerable<Match>> leagueFilter;
         private readonly ILeagueGenerator leagueGenerator;
+        private readonly ILogger logger;
 
         public FetchedLiveMatchConsumer(
             IBus messageBus,
             IDynamicRepository dynamicRepository,
             IFilter<IEnumerable<Match>, IEnumerable<Match>> leagueFilter,
-            ILeagueGenerator leagueGenerator)
+            ILeagueGenerator leagueGenerator,
+            ILogger logger)
         {
             this.messageBus = messageBus;
             this.dynamicRepository = dynamicRepository;
             this.leagueFilter = leagueFilter;
             this.leagueGenerator = leagueGenerator;
+            this.logger = logger;
         }
 
         public async Task Consume(ConsumeContext<ILiveMatchFetchedMessage> context)
@@ -67,9 +72,16 @@ namespace Soccer.EventProcessors.Matches
 
         private async Task InsertOrRemoveLiveMatches(Language language, IEnumerable<Match> newLiveMatches, IEnumerable<Match> removedMatches)
         {
-            var command = new InsertOrRemoveLiveMatchesCommand(language, newLiveMatches, removedMatches);
+            try
+            {
+                var command = new InsertOrRemoveLiveMatchesCommand(language, newLiveMatches, removedMatches);
 
-            await dynamicRepository.ExecuteAsync(command);
+                await dynamicRepository.ExecuteAsync(command);
+            }
+            catch (Exception ex)
+            {
+                await logger.ErrorAsync(ex.Message, ex);
+            }
         }
 
         private async Task PublishLiveMatchUpdatedMessage(Language language, IEnumerable<Match> newLiveMatches, IEnumerable<Match> removedMatches, int liveMatchCount)
