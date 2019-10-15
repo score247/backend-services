db2a3c149dc32441180526e50740d81e00e79f94
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-10-15 16:01:14 +0700

    Revert "121694 - Update write log before update team statistic"
    This reverts commit a0f2caa0cac1c6d1e246cb2f8e5e65caa70fd08d.

--- src/Soccer/Services/Soccer.EventProcessors/Teams/UpdateTeamStatisticConsumer.cs
+++ src/Soccer/Services/Soccer.EventProcessors/Teams/UpdateTeamStatisticConsumer.cs
@@ -2,23 +2,17 @@
 {
     using System.Threading.Tasks;
     using Fanex.Data.Repository;
-    using Fanex.Logging;
     using MassTransit;
-    using Newtonsoft.Json;
     using Soccer.Core.Teams.QueueMessages;
     using Soccer.Database.Matches.Commands;
 
     public class UpdateTeamStatisticConsumer : IConsumer<ITeamStatisticUpdatedMessage>
     {
         private readonly IDynamicRepository dynamicRepository;
-        private readonly ILogger logger;
 
-        public UpdateTeamStatisticConsumer(
-            IDynamicRepository dynamicRepository,
-            ILogger logger)
+        public UpdateTeamStatisticConsumer(IDynamicRepository dynamicRepository)
         {
             this.dynamicRepository = dynamicRepository;
-            this.logger = logger;
         }
 
         public async Task Consume(ConsumeContext<ITeamStatisticUpdatedMessage> context)
@@ -29,8 +23,6 @@
             {
                 return;
             }
-
-            await logger.InfoAsync($"Update match statistic {message.MatchId}, data: {JsonConvert.SerializeObject(message)}");
 
             var command = message.IsUpdateOnlyRedCard
                 ? (INonQueryCommand)new UpdateLiveMatchTeamRedCardCommand(message.MatchId, message.IsHome, message.TeamStatistic.RedCards, message.TeamStatistic.YellowRedCards)
