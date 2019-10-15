a0f2caa0cac1c6d1e246cb2f8e5e65caa70fd08d
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-10-15 13:00:16 +0700

    121694 - Update write log before update team statistic

--- src/Soccer/Services/Soccer.EventProcessors/Teams/UpdateTeamStatisticConsumer.cs
+++ src/Soccer/Services/Soccer.EventProcessors/Teams/UpdateTeamStatisticConsumer.cs
@@ -2,17 +2,23 @@
 {
     using System.Threading.Tasks;
     using Fanex.Data.Repository;
+    using Fanex.Logging;
     using MassTransit;
+    using Newtonsoft.Json;
     using Soccer.Core.Teams.QueueMessages;
     using Soccer.Database.Matches.Commands;
 
     public class UpdateTeamStatisticConsumer : IConsumer<ITeamStatisticUpdatedMessage>
     {
         private readonly IDynamicRepository dynamicRepository;
+        private readonly ILogger logger;
 
-        public UpdateTeamStatisticConsumer(IDynamicRepository dynamicRepository)
+        public UpdateTeamStatisticConsumer(
+            IDynamicRepository dynamicRepository,
+            ILogger logger)
         {
             this.dynamicRepository = dynamicRepository;
+            this.logger = logger;
         }
 
         public async Task Consume(ConsumeContext<ITeamStatisticUpdatedMessage> context)
@@ -23,6 +29,8 @@
             {
                 return;
             }
+
+            await logger.InfoAsync($"Update match statistic {message.MatchId}, data: {JsonConvert.SerializeObject(message)}");
 
             var command = message.IsUpdateOnlyRedCard
                 ? (INonQueryCommand)new UpdateLiveMatchTeamRedCardCommand(message.MatchId, message.IsHome, message.TeamStatistic.RedCards, message.TeamStatistic.YellowRedCards)
