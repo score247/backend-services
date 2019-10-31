797d526a62f43054f2cfff4c7f1ee8c58c541ae3
Egan Le <egan.le@nexcel.vn>
2019-10-31 08:49:35 +0700

    0000 - Update Get Active League SP

--- database/store-procedures/leagues/League_GetActive.sql
+++ database/store-procedures/leagues/League_GetActive.sql
@@ -2,5 +2,5 @@ DROP procedure IF EXISTS `League_GetActive`;
 
 CREATE DEFINER=`user`@`%` PROCEDURE `League_GetActive`()
 BEGIN
-	SELECT `Id`, `Name`, `Order`, `CountryCode` FROM `League` WHERE `IsActive` = '1';
+	SELECT `Id`, `Name`, `Order`, `Country` as 'CountryName', `CountryCode` FROM `League` WHERE `IsActive` = '1';
 END
