48e73efba0968e836adbb8fa917062dc610afdab
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-11-04 18:04:39 +0700

    000 - update connection string

--- database/DBUp.Deployment/Program.cs
+++ database/DBUp.Deployment/Program.cs
@@ -14,15 +14,15 @@ namespace DBUp.Deployment
         private static int Main(string[] args)
         {
             //LOCAL DEV
-            // var connectionString = "Data Source=10.18.200.109;Port=3386;Initial Catalog=score247_local_dev;Persist Security Info=True;User ID=user;Password=1234aa;Allow User Variables=True;";
-            //var connectionString = "Data Source=10.18.200.109;Port=3396;Initial Catalog=score247_local_test;Persist Security Info=True;User ID=root;Password=1234AA@PASS;Allow User Variables=True;";
-            // var connectionString = "Data Source=10.18.200.109;Port=3386;Initial Catalog=score247_local_dev2;Persist Security Info=True;User ID=user;Password=1234aa;Allow User Variables=True;";
+            //var connectionString = "Data Source=10.18.200.109;Port=3396;Initial Catalog=score247_local_dev;Persist Security Info=True;User ID=root;Password=1234AA@PASS;Allow User Variables=True;";
+            var connectionString = "Data Source=10.18.200.109;Port=3396;Initial Catalog=score247_local_test;Persist Security Info=True;User ID=root;Password=1234AA@PASS;Allow User Variables=True;";
+            // var connectionString = "Data Source=10.18.200.109;Port=3396;Initial Catalog=score247_local_dev2;Persist Security Info=True;User ID=root;Password=1234AA@PASS;Allow User Variables=True;";
 
             //LOCAL TEST
             // var connectionString = "Data Source=10.18.200.109;Port=3386;Initial Catalog=score247_local_test;Persist Security Info=True;User ID=user;Password=1234aa;Allow User Variables=True;";
 
             //LOCAL MAIN
-            var connectionString = "Data Source=10.18.200.109;Port=3386;Initial Catalog=score247_local_main;Persist Security Info=True;User ID=root;Password=1234AA@PASS;Allow User Variables=True;";
+            //var connectionString = "Data Source=10.18.200.109;Port=3386;Initial Catalog=score247_local_main;Persist Security Info=True;User ID=root;Password=1234AA@PASS;Allow User Variables=True;";
 
             var dir = new DirectoryInfo("../../../../store-procedures").FullName;
 
