ea4ec166ee4419dee12fe69cd2ae4fefc2a00455
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-11-08 16:34:02 +0700

    00000 - Update SP Get Active league

--- database/store-procedures/leagues/League_GetActive.sql
+++ database/store-procedures/leagues/League_GetActive.sql
@@ -1,6 +1,16 @@
 DROP procedure IF EXISTS `League_GetActive`;
 
-CREATE DEFINER=`user`@`%` PROCEDURE `League_GetActive`()
+CREATE DEFINER=`user`@`%` PROCEDURE `League_GetActive`(IN languageCode TINYTEXT)
 BEGIN
-	SELECT `Id`, `Name`, `Order`, `CategoryId`, `Country` as 'CountryName', `CountryCode`, `IsInternational`, `Region` FROM `League` WHERE `IsActive` = '1';
+	SELECT 
+		`Id`, 
+		`Name`,
+        `Order`, 
+        `CategoryId`,
+        `Country` as 'CountryName', 
+        `CountryCode`, 
+        `IsInternational`, 
+        `Region` 
+        FROM `League` as L
+        WHERE L.`IsActive` = '1' AND L.`Language` = languageCode;
 END
