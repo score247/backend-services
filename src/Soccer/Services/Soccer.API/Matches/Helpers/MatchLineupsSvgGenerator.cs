76b277320086480597aef068bdccebe8ea66312a
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-11-12 11:14:47 +0700

    123338 - Update event icons position

--- src/Soccer/Services/Soccer.API/Matches/Helpers/MatchLineupsSvgGenerator.cs
+++ src/Soccer/Services/Soccer.API/Matches/Helpers/MatchLineupsSvgGenerator.cs
@@ -124,7 +124,7 @@ namespace Soccer.API.Matches.Helpers
             for (int playerIndex = 0; playerIndex < totalPlayer; playerIndex++)
             {
                 var x = playerDistance * (playerIndex + 1);
-                var heightGap = rowIndex * yPlayerDistance + (int)(playerHeight * 1.5);
+                var heightGap = rowIndex * yPlayerDistance + (isHome ? 15 : 35);
                 var y = isHome ? heightGap : (stadiumHeight - heightGap);
 
                 playerElements.Add(RenderPlayerJersey(isHome, x, y));
