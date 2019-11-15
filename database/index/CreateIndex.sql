55b379890fac0984edab10b5096c73496fecb024
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-11-15 17:28:34 +0700

    000 - create index for H2H table

--- database/index/CreateIndex.sql
+++ database/index/CreateIndex.sql
@@ -1,1 +1,4 @@
 CREATE INDEX commentary_match_index ON Commentary (MatchId);
+
+CREATE INDEX headtohead_home_team_index ON HeadToHead (HomeTeamId);
+CREATE INDEX headtohead_away_team_index ON HeadToHead (AwayTeamId);
