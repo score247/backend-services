a9e752dafdf60f46ecd5f758448b41a9cab39568
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-11-18 17:24:17 +0700

    000 - remove select for share in LiveMatch_UpdateMatchResult

--- database/store-procedures/matches/LiveMatch_UpdateMatchResult.sql
+++ database/store-procedures/matches/LiveMatch_UpdateMatchResult.sql
@@ -5,27 +5,7 @@ CREATE DEFINER=`user`@`%` PROCEDURE `LiveMatch_UpdateMatchResult`(
     IN matchId VARCHAR(45), 
     IN matchResult TEXT)
 BEGIN
-	IF EXISTS (SELECT * FROM `Match` AS M WHERE M.Id = matchId FOR SHARE) 
-    THEN
-		 IF EXISTS (SELECT * FROM `LiveMatch` AS LM WHERE LM.Id = matchId FOR SHARE) 
-         THEN
 			UPDATE `LiveMatch` as LM
 			SET `Value` = JSON_SET(`Value`,  '$.MatchResult', JSON_EXTRACT(matchResult, '$'))
-			WHERE LM.`SportId` = sportId AND LM.Id = matchId;
-		 ELSE
-			 INSERT INTO `LiveMatch`
-			 SELECT 
-				Id, 
-				JSON_SET(`Value`,  '$.MatchResult', JSON_EXTRACT(matchResult, '$')) as `Value`,
-				`Language`,
-				`SportId`,
-				LeagueId,
-				EventDate,
-				Region,
-				now(),
-				now()
-				FROM `Match` as M
-			 WHERE M.`SportId` = sportId AND M.Id = matchId;
-		 END IF;
-    END IF;
+			WHERE LM.`SportId` = sportId AND LM.Id = matchId;	 
 END
