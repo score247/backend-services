a32df8715e1c258bed4f5acc1c9f920ebfa134e3
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-11-21 16:49:51 +0700

    124110 - update message pack key

--- src/Soccer/Soccer.Core/Leagues/Models/LeagueSeasonProcessedInfo.cs
+++ src/Soccer/Soccer.Core/Leagues/Models/LeagueSeasonProcessedInfo.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 
 namespace Soccer.Core.Leagues.Models
 {
-    [MessagePackObject(keyAsPropertyName: true)]
+    [MessagePackObject]
     public class LeagueSeasonProcessedInfo
     {
         [SerializationConstructor, JsonConstructor]
@@ -16,15 +16,21 @@ namespace Soccer.Core.Leagues.Models
             Fetched = Convert.ToBoolean(fetched);
             FetchedDate = fetchedDate;
         }
-
+#pragma warning disable S109 // Magic numbers should not be used
+        [Key(0)]
         public string LeagueId { get; }
 
+        [Key(1)]
         public string SeasonId { get; }
 
+        [Key(2)]
         public string Region { get; }
 
+        [Key(3)]
         public bool Fetched { get; }
 
+        [Key(4)]
         public DateTime FetchedDate { get; }
+#pragma warning restore S109 // Magic numbers should not be used
     }
 }
