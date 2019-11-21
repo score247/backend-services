d79d9ce913b7c9d478bccf009f7f3123d81b6d69
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-11-21 16:56:28 +0700

    124110 - update SerializationConstructor

--- src/Soccer/Soccer.Core/Leagues/Models/LeagueSeasonProcessedInfo.cs
+++ src/Soccer/Soccer.Core/Leagues/Models/LeagueSeasonProcessedInfo.cs
@@ -6,14 +6,23 @@ namespace Soccer.Core.Leagues.Models
 {
     [MessagePackObject]
     public class LeagueSeasonProcessedInfo
-    {
-        [SerializationConstructor, JsonConstructor]
+    {  
         public LeagueSeasonProcessedInfo(string leagueId, string seasonId, string region, sbyte fetched, DateTime fetchedDate) 
         {
             LeagueId = leagueId;
             SeasonId = seasonId;
             Region = region;
             Fetched = Convert.ToBoolean(fetched);
+            FetchedDate = fetchedDate;
+        }
+
+        [SerializationConstructor, JsonConstructor]
+        public LeagueSeasonProcessedInfo(string leagueId, string seasonId, string region, bool fetched, DateTime fetchedDate)
+        {
+            LeagueId = leagueId;
+            SeasonId = seasonId;
+            Region = region;
+            Fetched = fetched;
             FetchedDate = fetchedDate;
         }
 #pragma warning disable S109 // Magic numbers should not be used
