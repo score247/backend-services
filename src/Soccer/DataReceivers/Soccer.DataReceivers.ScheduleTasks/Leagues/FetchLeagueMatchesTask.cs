c4c2fe0ba6aed5abcf2bb8e2f750c3ccc0aac76e
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-11-22 15:33:10 +0700

    124110 - update fetchLeagueMatchesTask

--- src/Soccer/DataReceivers/Soccer.DataReceivers.ScheduleTasks/Leagues/FetchLeagueMatchesTask.cs
+++ src/Soccer/DataReceivers/Soccer.DataReceivers.ScheduleTasks/Leagues/FetchLeagueMatchesTask.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Hangfire;
 using MassTransit;
 using Score247.Shared.Enumerations;
+using Soccer.Core.Leagues.Models;
 using Soccer.Core.Matches.Events;
 using Soccer.Core.Matches.Models;
 using Soccer.Core.Shared.Enumerations;
@@ -17,26 +18,34 @@ namespace Soccer.DataReceivers.ScheduleTasks.Leagues
         [AutomaticRetry(Attempts = 1)]
         [Queue("low")]
         Task FetchLeagueMatches();
+
+        [AutomaticRetry(Attempts = 1)]
+        [Queue("low")]
+        Task FetchLeagueMatches(IList<LeagueSeasonProcessedInfo> leagueSeasons);
     }
     public class FetchLeagueMatchesTask : IFetchLeagueMatchesTask
     {
+        private const int NumberOfFetchInTask = 5;
+
         private readonly IAppSettings appSettings;
         private readonly IBus messageBus;
 
         private readonly ILeagueScheduleService leagueScheduleService;
         private readonly ILeagueSeasonService leagueSeasonService;
-
+        private readonly IBackgroundJobClient jobClient;
 
         public FetchLeagueMatchesTask(
             IBus messageBus,
             IAppSettings appSettings,
             ILeagueScheduleService leagueScheduleService,
-            ILeagueSeasonService leagueSeasonService)
+            ILeagueSeasonService leagueSeasonService,
+            IBackgroundJobClient jobClient)
         {
             this.appSettings = appSettings;
             this.messageBus = messageBus;
             this.leagueScheduleService = leagueScheduleService;
             this.leagueSeasonService = leagueSeasonService;
+            this.jobClient = jobClient;
         }
 
         public async Task FetchLeagueMatches()
@@ -48,12 +57,24 @@ namespace Soccer.DataReceivers.ScheduleTasks.Leagues
                 return;
             }
 
-            foreach (var season in unprocessedLeagueSeason)
+            for (var i = 0; i * NumberOfFetchInTask < unprocessedLeagueSeason.Count(); i++)
+            {
+                var batchOfLeague = unprocessedLeagueSeason.Skip(i * NumberOfFetchInTask).Take(NumberOfFetchInTask).ToList();
+
+                jobClient.Enqueue<IFetchLeagueMatchesTask>(t => t.FetchLeagueMatches(batchOfLeague));
+            }
+        }
+
+        public async Task FetchLeagueMatches(IList<LeagueSeasonProcessedInfo> leagueSeasons)
+        {
+            foreach (var season in leagueSeasons)
             {
                 foreach (var language in Enumeration.GetAll<Language>())
                 {
                     var matches = await leagueScheduleService.GetLeagueMatches(season.Region, season.LeagueId, language);
                     await PublishPreMatchesMessage(language, matches);
+
+                    //TODO schedule to fetch timelines, lineups, team results
                 }
             }
         }
---
+++ src/Soccer/DataReceivers/Tests/Soccer.DataReceivers.ScheduleTasks.Tests/Leagues/FetchLeagueMatchTaskTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Hangfire;
+using MassTransit;
+using NSubstitute;
+using Soccer.Core.Leagues.Models;
+using Soccer.DataProviders.Leagues;
+using Soccer.DataReceivers.ScheduleTasks.Leagues;
+using Soccer.DataReceivers.ScheduleTasks.Shared.Configurations;
+using Xunit;
+
+namespace Soccer.DataReceivers.ScheduleTasks.Tests.Leagues
+{
+    [Trait("Soccer.DataReceivers.ScheduleTasks", "FetchLeagueMatchTask")]
+    public class FetchLeagueMatchTaskTests
+    {
+        private readonly ILeagueScheduleService leagueScheduleService;
+        private readonly ILeagueSeasonService leagueSeasonService;
+        private readonly IBus messageBus;
+        private readonly IAppSettings appSettings;
+        private readonly IBackgroundJobClient jobClient;
+
+        private readonly FetchLeagueMatchesTask fetchLeagueMatchesTask;
+
+        public FetchLeagueMatchTaskTests() 
+        {
+            leagueScheduleService = Substitute.For<ILeagueScheduleService>();
+            leagueSeasonService = Substitute.For<ILeagueSeasonService>();
+            appSettings = Substitute.For<IAppSettings>();
+            messageBus = Substitute.For<IBus>();
+            jobClient = Substitute.For<IBackgroundJobClient>();
+
+            fetchLeagueMatchesTask = new FetchLeagueMatchesTask(messageBus, appSettings, leagueScheduleService, leagueSeasonService, jobClient);
+        }
+
+        [Fact]
+        public async Task FetchLeagueMatches_Always_GetUnprocessedLeagueSeason() 
+        {
+            // Arrange
+
+            // Act
+            await fetchLeagueMatchesTask.FetchLeagueMatches();
+
+            // Assert
+            leagueSeasonService.Received(1).GetUnprocessedLeagueSeason();
+        }
+
+    }
+}
