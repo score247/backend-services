16bc5d988ed1dcca5730647d0c392445898c5d0e
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-11-28 11:17:04 +0700

    000 - update dump files

--- database/dumps/124032-Sprint14.sql
+++ database/dumps/124032-Sprint14.sql
@@ -1,3 +1,20 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: 10.18.200.109    Database: score247_local_dev
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 --
 -- Table structure for table `Commentary`
 --
@@ -9,32 +26,30 @@ CREATE TABLE `Commentary` (
   `TimelineId` bigint(20) NOT NULL,
   `MatchId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Value` json NOT NULL,
-  `Language` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Language` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `CreatedTime` timestamp NOT NULL,
   `ModifiedTime` timestamp NOT NULL,
   `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `EventDate` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`Id`),
   KEY `commentary_match_index` (`MatchId`)
-) ENGINE=InnoDB AUTO_INCREMENT=214953 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=218431 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `Commentary_Archived`
+-- Table structure for table `EventSchedulerLog`
 --
 
-DROP TABLE IF EXISTS `Commentary_Archived`;
+DROP TABLE IF EXISTS `EventSchedulerLog`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Commentary_Archived` (
-  `TimelineId` bigint(20) NOT NULL,
-  `MatchId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Value` json NOT NULL,
-  `Language` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `CreatedTime` timestamp NOT NULL,
-  `ModifiedTime` timestamp NOT NULL,
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `EventSchedulerLog` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `EventName` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Status` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=210082 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=810 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,18 +81,37 @@ DROP TABLE IF EXISTS `League`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `League` (
-  `Id` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Id` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `Order` int(11) DEFAULT '10000',
-  `CategoryId` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `Country` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `Region` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `CategoryId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Country` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Region` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `IsActive` tinyint(4) DEFAULT '0',
-  `CountryCode` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `Language` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'en-US',
+  `CountryCode` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Language` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'en-US',
   `IsMajor` tinyint(4) DEFAULT '0',
   `IsInternational` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`Id`,`Language`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `LeagueSeason`
+--
+
+DROP TABLE IF EXISTS `LeagueSeason`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `LeagueSeason` (
+  `LeagueId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `SeasonId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Region` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Fetched` tinyint(4) DEFAULT '0',
+  `FetchedDate` timestamp NULL DEFAULT NULL,
+  `CreatedTime` timestamp NULL DEFAULT NULL,
+  `ModifiedTime` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`SeasonId`,`LeagueId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -91,26 +125,10 @@ DROP TABLE IF EXISTS `Lineups`;
 CREATE TABLE `Lineups` (
   `MatchId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Value` json NOT NULL,
-  `Language` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Language` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `CreatedTime` timestamp NOT NULL,
   `ModifiedTime` timestamp NOT NULL,
-  PRIMARY KEY (`MatchId`,`Language`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Lineups_Archived`
---
-
-DROP TABLE IF EXISTS `Lineups_Archived`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Lineups_Archived` (
-  `MatchId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Value` json NOT NULL,
-  `Language` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `CreatedTime` timestamp NOT NULL,
-  `ModifiedTime` timestamp NOT NULL,
+  `EventDate` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`MatchId`,`Language`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -130,6 +148,7 @@ CREATE TABLE `LiveMatch` (
   `LeagueId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `EventDate` timestamp NOT NULL,
   `Region` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `LeagueSeasonId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `CreatedTime` timestamp NULL DEFAULT NULL,
   `ModifiedTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`Id`,`Language`)
@@ -151,27 +170,7 @@ CREATE TABLE `Match` (
   `LeagueId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `EventDate` timestamp NOT NULL,
   `Region` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `CreatedTime` timestamp NULL DEFAULT NULL,
-  `ModifiedTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Id`,`Language`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Match_Archived`
---
-
-DROP TABLE IF EXISTS `Match_Archived`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Match_Archived` (
-  `Id` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Value` json NOT NULL,
-  `Language` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `SportId` int(11) NOT NULL,
-  `LeagueId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `EventDate` timestamp NOT NULL,
-  `Region` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `LeagueSeasonId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `CreatedTime` timestamp NULL DEFAULT NULL,
   `ModifiedTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`Id`,`Language`)
@@ -192,26 +191,28 @@ CREATE TABLE `Odds` (
   `BetTypeId` int(11) NOT NULL,
   `BookmakerId` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `EventDate` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1540956 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `Odds_Archived`
+-- Table structure for table `Standings`
 --
 
-DROP TABLE IF EXISTS `Odds_Archived`;
+DROP TABLE IF EXISTS `Standings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Odds_Archived` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `CreatedTime` timestamp NOT NULL,
+CREATE TABLE `Standings` (
+  `LeagueId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `SeasonId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Language` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `TableType` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Value` json NOT NULL,
-  `MatchId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `BetTypeId` int(11) NOT NULL,
-  `BookmakerId` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1605220 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `CreatedTime` timestamp NOT NULL,
+  `ModifiedTime` timestamp NOT NULL,
+  PRIMARY KEY (`LeagueId`,`SeasonId`,`Language`,`TableType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -225,27 +226,22 @@ CREATE TABLE `Timeline` (
   `Id` bigint(20) NOT NULL,
   `MatchId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Value` json NOT NULL,
-  `Language` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'en-US',
-  `CreatedTime` timestamp NULL DEFAULT NULL,
-  `ModifiedTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Id`,`MatchId`,`Language`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Timeline_Archived`
---
-
-DROP TABLE IF EXISTS `Timeline_Archived`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Timeline_Archived` (
-  `Id` bigint(20) NOT NULL,
-  `MatchId` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Value` json NOT NULL,
   `Language` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'en-US',
   `CreatedTime` timestamp NULL DEFAULT NULL,
   `ModifiedTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `EventDate` timestamp NULL DEFAULT NULL,
+  `Type` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`Id`,`MatchId`,`Language`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-28 11:05:58
