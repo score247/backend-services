96f4199a1e1587ae9d44ed031cc8d6aa4e621d58
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-12-02 17:41:13 +0700

    124751 - add Match_RemoveTimelines

---
+++ database/store-procedures/timelines/Match_RemoveTimelines.sql
@@ -0,0 +1,16 @@
+DROP procedure IF EXISTS `Match_RemoveTimelines`;
+
+CREATE DEFINER=`user`@`%` PROCEDURE `Match_RemoveTimelines`(
+	IN matchId varchar(50),
+    IN timelineIds TEXT)
+BEGIN
+    DECLARE i INT DEFAULT 0;     
+    DECLARE e INT DEFAULT JSON_LENGTH(timelineIds);
+    
+    WHILE i < e DO 
+		SET @DeleteId = JSON_UNQUOTE(JSON_EXTRACT(removedMatchIds, CONCAT('$[', i, '].Id')));         
+		DELETE FROM `Timeline` WHERE Id = @DeleteId AND MatchId = matchId;         
+        -- Increment the loop variable                                                                                                                                                        
+        SET i = i + 1;
+    END WHILE;     
+END
