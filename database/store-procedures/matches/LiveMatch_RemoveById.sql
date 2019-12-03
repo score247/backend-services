463f15476c97d6c4edb9f31fc5ac00bc693ffec9
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-12-03 17:28:38 +0700

    000 - remove LiveMatch_RemoveById

--- database/store-procedures/matches/LiveMatch_RemoveById.sql
+++
@@ -1,6 +0,0 @@
-DROP procedure IF EXISTS `LiveMatch_RemoveById`;
-
-CREATE DEFINER=`user`@`%` PROCEDURE `LiveMatch_RemoveById`(IN matchId varchar(50))
-BEGIN
-	DELETE FROM `LiveMatch` WHERE Id = matchId;
-END
