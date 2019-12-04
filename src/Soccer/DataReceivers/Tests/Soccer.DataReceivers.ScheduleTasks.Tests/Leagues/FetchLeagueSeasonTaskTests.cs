de948d4ba08e41a4e498a2f731f32601536b47c1
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-12-04 09:57:10 +0700

    000 - update unit tests

---
+++ src/Soccer/DataReceivers/Tests/Soccer.DataReceivers.ScheduleTasks.Tests/Leagues/FetchLeagueSeasonTaskTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MassTransit;
+using NSubstitute;
+using Soccer.DataProviders._Shared.Enumerations;
+using Soccer.DataProviders.Leagues;
+using Soccer.DataReceivers.ScheduleTasks.Shared.Configurations;
+using Xunit;
+
+namespace Soccer.DataReceivers.ScheduleTasks.Tests.Leagues
+{
+    [Trait("Soccer.DataReceivers.ScheduleTasks", "FetchLeagueSeasonTask")]
+    public class FetchLeagueSeasonTaskTests
+    {
+        private readonly Func<DataProviderType, ILeagueService> leagueServiceFactory;
+        private readonly IBus messageBus;
+        private readonly IAppSettings appSettings;
+
+        public FetchLeagueSeasonTaskTests() 
+        {
+            appSettings = Substitute.For<IAppSettings>();
+            messageBus = Substitute.For<IBus>();
+            leagueServiceFactory = Substitute.For<Func<DataProviderType, ILeagueService>>();
+        }
+    }
+}
---
+++ src/Soccer/Services/Tests/Soccer.EventProcessors.Tests/Leagues/FetchLeagueMatchesConsumerTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Fanex.Data.Repository;
+using MassTransit;
+using NSubstitute;
+using Soccer.Core.Leagues.Models;
+using Soccer.Core.Leagues.QueueMessages;
+using Soccer.Database.Leagues.Commands;
+using Soccer.EventProcessors.Leagues;
+using Xunit;
+
+namespace Soccer.EventProcessors.Tests.Leagues
+{
+    [Trait("Soccer.EventProcessors", "FetchLeagueMatchesConsumer")]
+    public class FetchLeagueMatchesConsumerTests
+    {
+        private readonly ConsumeContext<ILeagueMatchesFetchedMessage> context;
+        private readonly IDynamicRepository dynamicRepository;
+
+        private readonly FetchedLeagueMatchesConsumer consumer;
+
+        public FetchLeagueMatchesConsumerTests()
+        {
+            dynamicRepository = Substitute.For<IDynamicRepository>();
+            context = Substitute.For<ConsumeContext<ILeagueMatchesFetchedMessage>>();
+
+            consumer = new FetchedLeagueMatchesConsumer(dynamicRepository);
+        }
+
+        [Fact]
+        public async Task Consume_LeaguesSeasonFetchedMessage_EmptyLeagueSeason_NotExecuteAnyCommands()
+        {
+            context.Message.Returns(new LeagueMatchesFetchedMessage(
+                Enumerable.Empty<LeagueSeasonProcessedInfo>()
+                ));
+
+            await consumer.Consume(context);
+
+            await dynamicRepository.DidNotReceive().ExecuteAsync(Arg.Any<UpdateFetchedLeagueSeasonCommand>());
+        }
+
+        [Fact]
+        public async Task Consume_LeaguesSeasonFetchedMessage_ExecuteUpdateFetchedLeagueSeasonCommand()
+        {
+            context.Message.Returns(new LeagueMatchesFetchedMessage(
+                A.CollectionOfDummy<LeagueSeasonProcessedInfo>(5)
+                ));
+
+            await consumer.Consume(context);
+
+            await dynamicRepository.Received(1).ExecuteAsync(Arg.Any<UpdateFetchedLeagueSeasonCommand>());
+        }
+    }
+}
