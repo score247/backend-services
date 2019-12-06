e696c019f33f72bbb8b54892bd58306bc142969f
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-12-06 11:06:25 +0700

    124999 - add home page

---
+++ src/Soccer/Services/Soccer.API/_Shared/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Soccer.API._Shared.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return new ContentResult
+            {
+                Content = "Home Page"
+            };
+        }
+    }
+}
