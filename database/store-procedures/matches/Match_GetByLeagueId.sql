2a61e3f9f37713601a6a14936c13283433204466
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-12-07 11:43:12 +0700

    000 - add ModifiedTime when return fixtures

--- database/store-procedures/matches/Match_GetByLeagueId.sql
+++ database/store-procedures/matches/Match_GetByLeagueId.sql
@@ -2,7 +2,7 @@ DROP procedure IF EXISTS `Match_GetByLeagueId`;
 
 CREATE DEFINER=`user`@`%` PROCEDURE `Match_GetByLeagueId`(IN sportId INT, IN leagueId varchar(45), IN language TEXT)
 BEGIN
-	SELECT M.`Value`
+	SELECT JSON_REPLACE(`Match`.Value,  '$.ModifiedTime', `Match`.ModifiedTime) as `Value`
 		FROM `Match` as M  
 		WHERE M.LeagueId = leagueId
 		AND M.SportId = sportId
