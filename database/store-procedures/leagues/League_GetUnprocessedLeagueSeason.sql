1a7d4c10618d577adb6de4e1fc393776b4352d20
Vivian.Nguyen <vivian.nguyen@nexcel.vn>
2019-12-11 18:28:03 +0700

    000 - update League_GetUnprocessedLeagueSeason

--- database/store-procedures/leagues/League_GetUnprocessedLeagueSeason.sql
+++ database/store-procedures/leagues/League_GetUnprocessedLeagueSeason.sql
@@ -9,6 +9,6 @@ BEGIN
 			Season.`FetchedDate`
 		FROM LeagueSeason AS Season 
 		INNER JOIN League AS League ON Season.LeagueId = League.Id
-		WHERE League.IsActive = 1
+		WHERE League.IsMajor = 1
 		AND Season.Fetched = 0;
 END
