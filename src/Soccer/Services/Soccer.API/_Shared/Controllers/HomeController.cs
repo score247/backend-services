b71a1ecf31d290bddc774a73880888ec8b54b2e7
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-12-12 10:29:49 +0700

    124999 - add anonymous for home page

--- src/Soccer/Services/Soccer.API/_Shared/Controllers/HomeController.cs
+++ src/Soccer/Services/Soccer.API/_Shared/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,7 +7,7 @@ namespace Soccer.API._Shared.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return new ContentResult
