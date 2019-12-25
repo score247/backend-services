ede8b7a70dcaf7a4b176d40f1814c4bc8ace9d9c
Egan Le <egan.le@nexcel.vn>
2019-12-25 11:19:29 +0700

    Update sp UpdateGroupStages

--- database/data-update-scripts/League_UpdateGroupStage.sql
+++ database/data-update-scripts/League_UpdateGroupStage.sql
@@ -1,15 +1,34 @@
 INSERT INTO `score247_local_test`.`LeagueGroupStage`
 (`GroupStageName`,
 `LeagueId`,
+`LeagueSeasonId`,
+`Value`,
 `Language`,
 `CreatedTime`,
 `ModifiedTime`)
 SELECT
-	distinct M.Value -> "$.LeagueGroupName" as GroupStage, 
-	M.LeagueId, 	
-	M.Language, 
+	distinct JSON_UNQUOTE(JSON_EXTRACT(M.Value, "$.LeagueGroupName")) as GroupStage, 
+	M.LeagueId,
+    M.LeagueSeasonId,
+    CONCAT('{',
+		   CONCAT('"GroupStageName":',M.Value -> '$.LeagueGroupName',''),
+		   CONCAT(',"Language":"',M.Language,'"'),
+		   CONCAT(',"LeagueRound":',M.Value -> "$.LeagueRound",''),
+		   CONCAT(',"LeagueSeasonId":"',M.LeagueSeasonId,'"'),
+		   CONCAT(',"LeagueId":"',M.LeagueId,'"')
+		 ,'}') as GroupStageValue,
+	M.Language,
     now(),
     now()
 FROM score247_local_test.Match AS M
 WHERE M.Value -> "$.LeagueRound.Type.DisplayName" <> 'group'
-OR JSON_UNQUOTE(JSON_EXTRACT(M.Value, "$.LeagueRound.Group")) <> 'null';
+OR JSON_UNQUOTE(JSON_EXTRACT(M.Value, "$.LeagueRound.Group")) <> 'null'
+ON DUPLICATE KEY UPDATE
+	`Value` = CONCAT('{',
+		   CONCAT('"GroupStageName":',M.Value -> '$.LeagueGroupName',''),
+		   CONCAT(',"Language":"',M.Language,'"'),
+		   CONCAT(',"LeagueRound":',M.Value -> "$.LeagueRound",''),
+		   CONCAT(',"LeagueSeasonId":"',M.LeagueSeasonId,'"'),
+		   CONCAT(',"LeagueId":"',M.LeagueId,'"')
+		 ,'}'),
+	`ModifiedTime` = now();
