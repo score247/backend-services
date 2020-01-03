f92deb55d2957f649697a0b9f72fdccbb8f825e0
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2020-01-03 18:20:28 +0700

    000 - Update mysql sprint changes script

--- database/current-sprint-schema-changes/2.AlterTables.sql
+++ database/current-sprint-schema-changes/2.AlterTables.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `League`
-ADD COLUMN `HasGroups` TINYINT NOT NULL DEFAULT '0' AFTER `SeasonDates`;
 
 ALTER TABLE `League` 
 ADD COLUMN `Abbreviation` VARCHAR(30) NULL DEFAULT '' AFTER `Country`;
