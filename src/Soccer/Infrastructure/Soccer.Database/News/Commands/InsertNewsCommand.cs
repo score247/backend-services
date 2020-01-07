3268ed71bf6256ee8be93024552a1034035bc1b2
Vivian Nguyen <vivian.nguyen@spvn.local>
2020-01-07 14:17:37 +0700

    126611 - update consumer

---
+++ src/Soccer/Infrastructure/Soccer.Database/News/Commands/InsertNewsCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Score247.Shared.Enumerations;
+using Soccer.Core.News.Models;
+using Soccer.Core.Shared.Enumerations;
+
+namespace Soccer.Database.News.Commands
+{
+    public class InsertNewsCommand : BaseCommand
+    {
+        public InsertNewsCommand(
+            IEnumerable<NewsItem> newsList,
+            Language language) 
+        {
+            SportId = Sport.Soccer.Value;
+            News = ToJsonString(newsList);
+            Language = language.DisplayName;
+        }
+
+        public byte SportId { get; }
+
+        public string News { get; }
+
+        public string Language { get; }
+
+        public override string GetSettingKey() => "News_InsertOrUpdateNews";
+
+        public override bool IsValid() => !string.IsNullOrWhiteSpace(News) && !string.IsNullOrWhiteSpace(Language);
+    }
+}
---
+++ src/Soccer/Services/Soccer.EventProcessors/News/FetchNewsConsumer.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Fanex.Data.Repository;
+using MassTransit;
+using Soccer.Core.News.QueueMessages;
+using Soccer.Database.News.Commands;
+
+namespace Soccer.EventProcessors.News
+{
+    public class FetchNewsConsumer : IConsumer<INewsFetchedMessage>
+    {
+        private readonly IDynamicRepository dynamicRepository;
+
+        public FetchNewsConsumer(
+            IDynamicRepository dynamicRepository)
+        {
+            this.dynamicRepository = dynamicRepository;
+        }
+
+        public async Task Consume(ConsumeContext<INewsFetchedMessage> context)
+        {
+            if (context?.Message?.NewsList == null || context.Message.NewsList.Any())
+            {
+                return;
+            }
+
+            await dynamicRepository.ExecuteAsync(new InsertNewsCommand(context.Message.NewsList, context.Message.Language));
+        }
+    }
+}
