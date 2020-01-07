5283e9844c98b8dfa38b29e1aec70fd4eba585b7
Vivian Nguyen <vivian.nguyen@spvn.local>
2020-01-07 16:26:35 +0700

    126611 - update insert sp

--- database/store-procedures/news/News_InsertOrUpdateNews.sql
+++ database/store-procedures/news/News_InsertOrUpdateNews.sql
@@ -7,7 +7,7 @@ BEGIN
     
     WHILE i < e DO
        INSERT INTO `News`
-		(`Title`, `SportId`, `Language`, `Content`, `ImageSource`, `Link`, `PublishedDate`)
+		(`Title`, `SportId`, `Language`, `Content`, `ImageSource`, `Link`, `PublishedDate`, `CreatedTime`)
 		VALUES
 			(
 			JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].Title'))),
@@ -16,12 +16,13 @@ BEGIN
 			JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].Content'))),
 			JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].ImageSource'))),
 			JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].Link'))),
-            JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].PublishedDate'))))
+            STR_TO_DATE(JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].PublishedDate'))),'%Y-%m-%dT%H:%i:%s+00:00'),
+            now())
 		ON DUPLICATE KEY UPDATE
             Title = JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].Title'))),
             Content = JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].Content'))),
             ImageSource = JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].ImageSource'))),
-            PublishedDate = JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].PublishedDate')));
+            PublishedDate = STR_TO_DATE(JSON_UNQUOTE(JSON_EXTRACT(news, CONCAT('$[', i, '].PublishedDate'))),'%Y-%m-%dT%H:%i:%s+00:00');
         -- Increment the loop variable                                                                                                                                                        
         SET i = i + 1;
     END WHILE;
