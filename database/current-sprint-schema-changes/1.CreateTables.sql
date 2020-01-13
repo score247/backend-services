5149495c16d0a5e90dfc1175ac2a48e6a0dfe6bb
Vivian Nguyen <vivian.nguyen@spvn.local>
2020-01-13 10:31:20 +0700

    126189 - update news table

--- database/current-sprint-schema-changes/1.CreateTables.sql
+++ database/current-sprint-schema-changes/1.CreateTables.sql
@@ -1,13 +1,14 @@
 DROP TABLE IF EXISTS `News`;
-CREATE TABLE `News` (  
-  `Title` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+CREATE TABLE `News` (
+  `Title` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `SportId` int(11) NOT NULL,
-  `Language` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Content` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ImageSource` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Link` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Language` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ImageSource` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Link` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Author` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `PublishedDate` timestamp NOT NULL,
   `CreatedTime` timestamp NOT NULL,
   `ModifiedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Link`,`Language`)
+  PRIMARY KEY (`Language`,`Link`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
