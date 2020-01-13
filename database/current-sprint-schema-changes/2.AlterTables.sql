0956512d4df3a7746dd92c33bfa270164b4266a9
Vivian Nguyen <vivian.nguyen@spvn.local>
2020-01-13 19:11:51 +0700

    000 - update sprint changes

--- database/current-sprint-schema-changes/2.AlterTables.sql
+++
@@ -1,6 +0,0 @@
-
-ALTER TABLE `League` 
-ADD COLUMN `Abbreviation` VARCHAR(30) NULL DEFAULT '' AFTER `Country`;
-
-ALTER TABLE `LeagueGroupStage` 
-ADD COLUMN `GroupName` VARCHAR(250) NULL DEFAULT '' AFTER `GroupStageName`;
