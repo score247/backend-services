10513946add49dcd40ef47fc6793200c85ed91bf
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2020-01-15 18:29:37 +0700

    000 - add log to health check service

--- src/Soccer/DataReceivers/Soccer.DataReceivers.ScheduleTasks/_Shared/HealthCheck/HealthCheckService.cs
+++ src/Soccer/DataReceivers/Soccer.DataReceivers.ScheduleTasks/_Shared/HealthCheck/HealthCheckService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Fanex.Logging;
 
 namespace Soccer.DataReceivers.ScheduleTasks._Shared.HealthCheck
 {
@@ -13,15 +14,18 @@ namespace Soccer.DataReceivers.ScheduleTasks._Shared.HealthCheck
     public class HealthCheckService : IHealthCheckService
     {
         private readonly Dictionary<string, DateTime> healthCheckContainer;
+        private readonly ILogger logger;
 
-        public HealthCheckService()
+        public HealthCheckService(ILogger logger)
         {
             healthCheckContainer = new Dictionary<string, DateTime>();
+            this.logger = logger;
         }
 
         public void HeartBeat(string name)
         {
             healthCheckContainer[name] = DateTime.Now;
+            logger.Info("HealthCheckService: " + name);
         }
 
         public Dictionary<string, DateTime> LatestHeartBeats() => healthCheckContainer;
