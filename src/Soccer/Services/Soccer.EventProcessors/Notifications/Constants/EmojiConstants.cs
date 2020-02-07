ee01cc0107d2008de370c0e9c870bffc75cc2bcb
Vivian Nguyen <vivian.nguyen@spvn.local>
2020-02-07 16:14:29 +0700

    128009 - update emoji

--- src/Soccer/Services/Soccer.EventProcessors/Notifications/Constants/EmojiConstants.cs
+++ src/Soccer/Services/Soccer.EventProcessors/Notifications/Constants/EmojiConstants.cs
@@ -3,8 +3,10 @@
     public static class EmojiConstants
     {
         public const string SOCCER_BALL_ICON = "\u26BD";
-        public const string SOUND_ICON = "\uF50A";
-        public const string FLAG_ICON = "\uF3C1";
+        public const int SOUND_ICON = 0x1F50A; 
+        public const string FLAG_ICON = "\u1F3C1";
         public const string TROPHY_ICON = "\uF3C6";
+
+        public static string ConvertIcon(int icon) => char.ConvertFromUtf32(icon);
     }
 }
