6ac8b9270a845ee14cd7353d578a4d55a1891ed2
Vivian Nguyen <vivian.nguyen@spvn.local>
2020-02-20 11:45:33 +0700

    128754 - add GetTrendingTeamsCriteria and SearchTeamByNameCriteria

---
+++ src/Soccer/Infrastructure/Soccer.Database/Teams/Criteria/GetTrendingTeamsCriteria.cs
@@ -0,0 +1,23 @@
+﻿using Fanex.Data.Repository;
+using Score247.Shared.Enumerations;
+using Soccer.Core.Shared.Enumerations;
+
+namespace Soccer.Database.Teams.Criteria
+{
+    public class GetTrendingTeamsCriteria : CriteriaBase
+    {
+        public GetTrendingTeamsCriteria(Language language)
+        {
+            Language = language.DisplayName;
+            SportId = Sport.Soccer.Value;
+        }
+
+        public int SportId {get;}
+
+        public string Language { get; }
+
+        public override string GetSettingKey() => "Team_GetTrendingTeams";
+
+        public override bool IsValid() => !string.IsNullOrWhiteSpace(Language);
+    }
+}
---
+++ src/Soccer/Infrastructure/Soccer.Database/Teams/Criteria/SearchTeamByNameCriteria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Fanex.Data.Repository;
+using Score247.Shared.Enumerations;
+using Soccer.Core.Shared.Enumerations;
+
+namespace Soccer.Database.Teams.Criteria
+{
+    public class SearchTeamByNameCriteria : CriteriaBase
+    {
+        public SearchTeamByNameCriteria(string keyword, Language language)
+        {
+            Keyword = keyword;
+            Language = language.DisplayName;
+            SportId = Sport.Soccer.Value;
+        }
+
+        public int SportId { get; }
+
+        public string Language { get; }
+
+        public string Keyword { get; }
+
+        public override string GetSettingKey() => "Team_SearchByName";
+
+        public override bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
